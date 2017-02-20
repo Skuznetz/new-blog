c509516874a5ba07b9e4fed4dae58210aeb7986a
Skuznetz <s.kuznetzov@mail.ru>
2017-02-20 16:28:16 +0200

    show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+	root 'posts#index'
+	resources :posts
   devise_for :users
 
-resource :posts
-root 'posts#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
